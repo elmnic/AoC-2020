0bf0d6632e5da16bef03ce691091bde8074019b5
Niclas Elmäng <niclas.elmang@outlook.com>
2020-12-06 17:48:50 +0100

    Refactor Day 6

--- Day 6/day6.scala
+++ Day 6/day6.scala
@@ -10,41 +10,20 @@ object day6 {
 
 		var part1 = 0
 		var part2 = 0
-		var questions1 = scala.collection.mutable.Set[Char]()
-		var questions2 = scala.collection.mutable.Map[Char, Int]()
-		var counter = 0
+		var container = List[List[Char]]()
 		for (line <- source.getLines()) {
 			
-			// There are answers
-			if (!line.isEmpty()) {
-
-				// Increment number of respondants
-				counter += 1
-
-				// Loop through the answers
-				for (char <- line) {
-				
-					// Part 1, Add to set for non-duplicate checking
-					questions1.add(char)
-
-					// Part 2, Increment count of answer
-					if (questions2.contains(char))
-						questions2 += (char -> (questions2(char) + 1))
-					else
-						questions2 += (char -> 1)
-				}
-			}
+			if (!line.isEmpty()) 
+				container = container :+ line.toList
 			else {
 
-				// Part 1, Add sum of unique answers 
-				part1 += questions1.size
-				questions1.clear()
-
-				// Part 2, Add sum of answers that every person answered
-				part2 += questions2.values.count(_ == counter)
-				questions2.clear()
-
-				counter = 0
+				// Part 1, Count number of unique questions answered
+				part1 += container.reduce(_ union _).distinct.size
+				
+				// Part 2, Count number of questions answered by everyone
+				part2 += container.reduce(_ intersect _).distinct.size
+				
+				container = List()
 			}
 			
 		}
